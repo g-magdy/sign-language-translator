a83005c8dfb6eb0ab0f15112f5f0f9ab3c7a61f6
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-10-18 09:40:29 +0300

    progress: classification working

--- app.py
+++ app.py
@@ -3,6 +3,24 @@ from flask import Flask, render_template, request, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from PIL import Image
 
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model = load_model('./models/testvgg_19.h5')
+
+def get_label(code):
+    labels = {
+        0: 'buildings',
+        1: 'forest',
+        2: 'glacier',
+        3: 'mountain',
+        4: 'sea',
+        5: 'street'
+    }
+    
+    return labels[code]
+
+
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 
@@ -50,6 +68,46 @@ def upload_photo():
             
             return render_template("upload.html", filename='greyscale_' + filename)
             
+            
+@app.route("/classify", methods=['GET', 'POST'])
+def classify_img():
+    if request.method == "GET":
+        return render_template("classify.html", show_result=False)
+    else:
+        
+        if 'file' not in request.files:
+            return "No file was uploaded"
+        
+        file = request.files['file']
+        
+        if file.filename == "":
+            return "No file was selected"
+        
+        if file and file_is_allowed(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            
+            # This should not be done in real servers
+            # but for the sake of simplicity
+            file.save(filepath)
+            
+            image = Image.open(filepath)
+            
+            # TODO: Apply processing here
+            image = image.resize((150, 150))
+            image_array = np.array(image) 
+            image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
+            
+             # Make a prediction
+            prediction = model.predict(image_array)
+        
+            # Example: Get the predicted class (assuming the model outputs a single value)
+            predicted_class_code = np.argmax(prediction)
+            
+            predicted_class = get_label(predicted_class_code)
+            
+            return render_template("classify.html", show_result=True, predicted_class=predicted_class)
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
