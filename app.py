9e62a2ee3fb18960aa935db1d71f7a82b6025b78
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-10-18 08:04:45 +0300

    progress: how to upload

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_photo():
+    if request.method == "GET":
+        return render_template("upload.html")
+    else:
+        return "TODO: post part"
+
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
