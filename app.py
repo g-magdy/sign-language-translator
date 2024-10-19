7ec0d4647db0ac2d8a052557a47ec662661686e4
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-10-19 04:26:27 +0300

    remove unneeded pages

--- app.py
+++ app.py
@@ -16,18 +16,6 @@ arabic_onehot_encoder = joblib.load("./models/arabic_encoder.pkl")
 english_model = load_model('./models/english_model.h5')
 english_onehot_encoder = joblib.load("./models/english_encoder.pkl")
 
-def get_label(code):
-    labels = {
-        0: 'buildings',
-        1: 'forest',
-        2: 'glacier',
-        3: 'mountain',
-        4: 'sea',
-        5: 'street'
-    }
-    
-    return labels[code]
-
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
@@ -44,81 +32,8 @@ def index():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-@app.route("/upload", methods=['GET', 'POST'])
-def upload_photo():
-    if request.method == "GET":
-        return render_template("upload.html")
-    else:
-        
-        if 'file' not in request.files:
-            return "No file was uploaded"
-        
-        file = request.files['file']
-        
-        if file.filename == "":
-            return "No file was selected"
-        
-        if file and file_is_allowed(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            
-            # This should not be done in real servers
-            # but for the sake of simplicity
-            file.save(filepath)
-            
-            image = cv2.imread(filepath)
-            image_resized = cv2.resize(image, (224, 224))
-            gray_image = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
-            edges = cv2.Canny(gray_image, threshold1=70, threshold2=70)
-            
-            edges_img_path = os.path.join(app.config['UPLOAD_FOLDER'], 'e_' + filename)
-            cv2.imwrite(filename=edges_img_path, img=edges)
-            
-            return render_template("upload.html", filename='e_' + filename)
-            
-            
-@app.route("/classify", methods=['GET', 'POST'])
-def classify_img():
-    if request.method == "GET":
-        return render_template("classify.html", show_result=False)
-    else:
-        
-        if 'file' not in request.files:
-            return "No file was uploaded"
-        
-        file = request.files['file']
-        
-        if file.filename == "":
-            return "No file was selected"
-        
-        if file and file_is_allowed(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            
-            # This should not be done in real servers
-            # but for the sake of simplicity
-            file.save(filepath)
-            
-            image = Image.open(filepath)
-            
-            # TODO: Apply processing here
-            image = image.resize((150, 150))
-            image_array = np.array(image) 
-            image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
-            
-             # Make a prediction
-            prediction = vgg19_model.predict(image_array)
-        
-            # Example: Get the predicted class (assuming the model outputs a single value)
-            predicted_class_code = np.argmax(prediction)
-            
-            predicted_class = get_label(predicted_class_code)
-            
-            return render_template("classify.html", show_result=True, predicted_class=predicted_class)
-    
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)            
+                
 @app.route("/arabic", methods=['GET', 'POST'])
 def arabic_classification():
     if request.method == "GET":
