fdfff5ddf5fd1ddd69d8e4a8985f9589d3e2fb1b
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-10-19 05:35:22 +0300

    refactor: remove unneeded templates and routes

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from PIL import Image
 import joblib
 import numpy as np
 from tensorflow.keras.models import load_model #type: ignore
+from transformers import MarianMTModel, MarianTokenizer
+from consts import single_char_translations
 
-vgg19_model = load_model('./models/testvgg_19.h5')
 
 arabic_model = load_model('./models/arabic_model.h5')
 arabic_encoder = joblib.load("./models/arabic_encoder.pkl")
@@ -59,6 +60,37 @@ def get_image_prediction(file, model, encoder) -> str:
     
     return filename, predicted_class_label            
 
+def translate_text(input_text, source_lang, target_lang):
+    # Check if input_text is a single character and translate manually
+    if source_lang=='en':
+        if len(input_text) == 1 and input_text.lower() in single_char_translations:
+            return single_char_translations[input_text.lower()]
+
+    # For words and sentences, use MarianMT for translation
+    model_name = f"Helsinki-NLP/opus-mt-{source_lang}-{target_lang}"
+    
+    # Load the pre-trained model and tokenizer
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+
+    # Ensure input_text is a string or flatten it if it's an array
+    if isinstance(input_text, np.ndarray):
+        input_text = input_text.flatten().astype(str).tolist()  # Convert to list of strings
+        input_text = ' '.join(input_text)  # Join list into a single string
+    elif isinstance(input_text, list):
+        input_text = ' '.join(input_text)
+    elif not isinstance(input_text, str):
+        raise ValueError("Input text must be a string, a list of strings, or a numpy.ndarray.")
+    
+    # Encode input text
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+
+    # Translate text with max_new_tokens
+    output_ids = model.generate(input_ids, max_new_tokens=50)
+    translated_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    
+    return translated_text.lower()
+
 
 @app.route('/')
 def index():
@@ -70,10 +102,14 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)            
 
                 
-@app.route("/arabic", methods=['GET', 'POST'])
-def arabic_classification():
+@app.route("/<language>", methods=['GET', 'POST'])
+def classification(language):
+    
+    if language not in ['arabic', 'english']:
+        return "Invalid language", 404
+    
     if request.method == "GET":
-        return render_template("arabic_classification.html", show_result=False)
+        return render_template(f"classification.html", show_result=False, language=language)
     else:
         
         if 'file' not in request.files:
@@ -85,31 +121,14 @@ def arabic_classification():
             return "No file was selected"
         
         if file and file_is_allowed(file.filename):
-            name, predicted_class_label = get_image_prediction(file=file, model=arabic_model, encoder=arabic_encoder)
             
-            return render_template("arabic_classification.html", show_result = True, predicted_class=predicted_class_label, edges_image=name)
-        
-
-@app.route("/english", methods=['GET', 'POST'])
-def english_classification():
-    if request.method == "GET":
-        return render_template("english_classification.html", show_result=False)
-    else:
-        
-        if 'file' not in request.files:
-            return "No file was uploaded"
-        
-        file = request.files['file']
-        
-        if file.filename == "":
-            return "No file was selected"
-        
-        if file and file_is_allowed(file.filename):
-            name, predicted_class_label = get_image_prediction(file=file, model=english_model, encoder=english_encoder)
+            model = arabic_model if language == "arabic" else english_model
+            encoder = arabic_encoder if language == "arabic" else english_encoder
             
-            return render_template("english_classification.html", show_result = True, predicted_class=predicted_class_label, edges_image=name)
+            name, predicted_class_label = get_image_prediction(file=file, model=model, encoder=encoder)
+            
+            return render_template(f"classification.html", show_result = True, predicted_class=predicted_class_label, edges_image=name, language=language)
         
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
